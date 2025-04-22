ba258254cce59593800611b82610d15fb1fcaf0a
retisha256 <retishanakiyemba202@gmail.com>
2025-04-22 19:46:43 +0300

    added two links of view and delete to allsales.html

--- kgl_app/forms.py
+++ kgl_app/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from .models import *
 
+
 class AddStockForm(ModelForm):
     class Meta:
         model=Stock
--- kgl_app/views.py
+++ kgl_app/views.py
@@ -3,6 +3,10 @@ from .models import *
 
 # Create your views here.
 def index(request):
+
+        
+    
+        
     return render(request, 'index.html')
 
 def dash(request):
